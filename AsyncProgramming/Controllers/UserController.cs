2a7cb7c1d8e71a4e36d25ab2dfdab7f41e55e75c
Yuliia Stakhiv <Yuliia_Stakhiv@epam.com>
2020-02-13 13:30:58 +0200

    Add project files.

---
+++ AsyncProgramming/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyncProgramming.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var client = new HttpClient();
+            var message = await client.GetStringAsync("https://randomuser.me/api/");
+            return message;
+        }
+    }
+}
