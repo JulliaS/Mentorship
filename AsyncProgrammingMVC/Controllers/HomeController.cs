b06e2ccc9a6fe7ce1f1c03aea97fdd5d2f7b5da5
Yuliia Stakhiv <Yuliia_Stakhiv@epam.com>
2020-02-25 16:59:18 +0200

    Add : async programming project with UI

---
+++ AsyncProgrammingMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection.Metadata;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AsyncProgrammingMVC.Models;
+
+namespace AsyncProgrammingMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private const string url = "https://www.w3schools.com/howto/howto_js_progressbar.asp";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> LoadData()
+        {
+            var client = new HttpClient();
+            var time = 2000;
+
+            Thread.Sleep(time);
+            var message = await client.GetStringAsync(url);
+
+            return View("Index", message);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
